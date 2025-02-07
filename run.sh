0be0ac833067c4dc35d00d8703379c72d13a31c9
mvle <mvle>
2025-02-07 10:44:15 -0600

    fixup result dir and script path

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # disable CPU frequency changes
-echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
-echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+#echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
+#echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 
-cd /home/mvle/git/github.com/LEBench/
-export LEBENCH_DIR=/home/mvle/git/github.com/LEBench/
-python2 run.py
+cd /home/root/LEBench/
+export LEBENCH_DIR=/home/root/LEBench/
+python3 run.py
